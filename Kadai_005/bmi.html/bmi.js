700ad545ea5c115a7397201e7ecf36895efc1b3e
nandemocandemo333 <masvaletardequenunca128@gmail.com>
2024-12-30 22:17:18 +0900

    Add files via upload

---
+++ Kadai_005/bmi.html/bmi.js
@@ -0,0 +1,12 @@
+//変数の宣言
+let userName;
+//値の代入
+userName = 'SAMURAI';
+let userWeight = 68;
+let userHeight = 1.7;
+console.log(userWeight);
+console.log(userHeight);
+//変数の宣言・値の代入
+let bmi = userWeight / (userHeight * userHeight);
+//BMIの計算式の値を出力
+console.log(bmi)
