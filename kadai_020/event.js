72eb1e5f429d015e48b9e7f64596ed3cec3b4c4d
nandemocandemo333 <masvaletardequenunca128@gmail.com>
2025-01-11 14:42:06 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById ('btn');
+
+const text =document.getElementById ('text');
+
+btn.addEventListener('click' , () => {
+    //const li =document.createElement ('li');
+
+    //li.textContent ='ボタンをクリックしました';
+
+    text.textContent ='ボタンをクリックしました';
+    
+});
